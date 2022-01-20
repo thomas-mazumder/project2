fc61ff48b6e2b0007d0c321770c4e16da0a00314
thomas-mazumder <thomas.mazumder@ucsf.edu>
2022-01-19 23:34:03 -0800

    did work

--- search/graph.py
+++ search/graph.py
@@ -11,6 +11,13 @@ class Graph:
         
         """
         self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
+    
+    def shortest_path(self, start, end, pred):
+        path = []
+        path.append(end)
+        while path[-1] != start:
+            path.append(pred[path[-1]])
+        return path[::-1]
 
     def bfs(self, start, end=None):
         """
@@ -19,8 +26,31 @@ class Graph:
         * If there's no end node, just return a list with the order of traversal
         * If there is an end node and a path exists, return a list of the shortest path
         * If there is an end node and a path does not exist, return None
-
         """
+        visited = dict(zip(list(self.graph.nodes),[False]*len(self.graph.nodes)))
+        queue = list()
+        pred = dict()
+        traversal = []
+        
+        queue.append(start)
+        visited[start] = True
+        
+        while len(queue) != 0:
+            v = queue.pop(0)
+            traversal.append(v)
+            for successor in self.graph.successors(v):
+                if not visited[successor]:
+                    visited[successor] = True
+                    pred[successor] = v
+                    queue.append(successor)
+                    
+                    if successor == end:
+                        return self.shortest_path(start, end, pred)
+                    
+        if end == None:
+            return traversal
+        else:
+            return None
         return
 
 
